918a1c8f02e65e1fb55af8178af5978057c8af92
BorbelyBalazs <blaze82bass@gmail.com>
2023-01-02 09:43:26 +0100

    seconCommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int[] arr = new int[100];
+
+       for (int i = 0; i < arr.length; i++) {
+           arr[i] = (int) (Math.random()*100)+1;
+       }
     }
 }
