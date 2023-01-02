7d48ab2aff08cc3da6526efb3ca201bcfcc8945f
BorbelyBalazs <blaze82bass@gmail.com>
2023-01-02 09:57:09 +0100

    sumEvens és getMin függvények megírva

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
        int[] arr = new int[20];
@@ -5,5 +7,23 @@ public class Main {
        for (int i = 0; i < arr.length; i++) {
            arr[i] = (int) (Math.random()*100)+1;
        }
+
+        System.out.println(getMin(arr));
+        System.out.println(sumEvens(arr));
+    }
+    public static int getMin(int[] arr) {
+        Arrays.sort(arr);
+        return arr[0];
+    }
+
+    public static int sumEvens(int[] arr) {
+        int counter = 0;
+
+        for (var i: arr) {
+            if (i % 2 == 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
