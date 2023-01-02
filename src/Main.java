20bc8b2f19f236520d7c5b2d19e2336ae67c3e34
BorbelyBalazs <blaze82bass@gmail.com>
2023-01-02 09:44:57 +0100

    A tömböt 20-ra módosítottam

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       int[] arr = new int[100];
+       int[] arr = new int[20];
 
        for (int i = 0; i < arr.length; i++) {
            arr[i] = (int) (Math.random()*100)+1;
