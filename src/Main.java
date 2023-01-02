6d9c492d7e99f39d4775105e43a8c63e2ef869d4
BorbelyBalazs <blaze82bass@gmail.com>
2023-01-02 10:17:06 +0100

    Véglegesítettem a fájlt (asszem :) )

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,14 @@ public class Main {
         }
 
         System.out.println(getMin(arr));
+        System.out.println(getMax(arr));
         System.out.println(countEvens(arr));
-        System.out.println(getMin(arr));
+        System.out.println(countOdds(arr));
+        System.out.println(sumEvens(arr));
         System.out.println(sumOdds(arr));
     }
+
+
 
     public static int getMin(int[] arr) {
         Arrays.sort(arr);
@@ -36,10 +40,21 @@ public class Main {
     }
 
     public static int sumOdds(int[] arr) {
-        return Arrays.stream(arr).filter(a -> a % 2 == 0).sum();
+        return Arrays.stream(arr).filter(a -> a % 2 != 0).sum();
     }
 
     public static int countOdds(int[] arr){
-        return (int) Arrays.stream(arr).filter(a -> a%2 == 0).count();
+        return (int) Arrays.stream(arr).filter(a -> a%2 != 0).count();
+    }
+
+    private static int sumEvens(int[] arr) {
+        int sum = 0;
+
+        for (var i: arr) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
