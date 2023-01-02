0f6bd94ddb3f7f9fd19eaa27ecb9215930910f48
marinchuck <marincsakt@gmail.com>
2023-01-02 09:50:01 +0100

    add min function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
        int[] arr = new int[20];
@@ -5,5 +7,9 @@ public class Main {
        for (int i = 0; i < arr.length; i++) {
            arr[i] = (int) (Math.random()*100)+1;
        }
+    }
+
+    public static int min(int[] arr){
+      return Arrays.stream(arr).min().getAsInt();
     }
 }
